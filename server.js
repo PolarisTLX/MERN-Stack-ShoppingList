14a082f4a05fe67ef38959a342aa728c217689f4
PolarisTLX <paul.a.rail@gmail.com>
2018-08-09 11:47:19 -0400

    deploying full-stack app to heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+// to be able to deploy to Heroku:
 const path = require('path');
 
 const items = require('./routes/api/items');
@@ -23,6 +24,17 @@ mongoose.connect(db)
 
 // Use Routes
 app.use('/api/items', items);
+
+// to be able to deploy to Heroku:
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
